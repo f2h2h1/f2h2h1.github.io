de67cc93b7b7723fc6317529ee4bedac4617270d
f2h2h1 <mP6iB1hB@outlook.com>
2022-03-17 19:55:30 +0800

    update auxiliary 尝试实现 github page 的 url 重写

---
+++ index.js
@@ -0,0 +1,485 @@
+'use strict';
+var Application = (function(){
+    let application = function(params){};
+    var articleList = {};
+    function ajax(options) {
+        options = options || {};
+        options.type = (options.type || 'GET').toUpperCase();
+        if (!options.url) {
+            return;
+        }
+        let params = options.data || '';
+        if (window.XMLHttpRequest) {
+            var xhr = new XMLHttpRequest();
+        } else {
+            var xhr = new ActiveXObject('Microsoft.XMLHTTP');
+        }
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                var status = xhr.status;
+                if (status >= 200 && status < 300) {
+                    let result = xhr.responseText;
+                    if (options.dataType && options.dataType == 'json') {
+                        result = JSON.parse(result);
+                    }
+                    options.success && options.success(result, xhr);
+                } else {
+                    options.error && options.error(status, xhr.responseText, options.error);
+                }
+                options.complete && options.complete();
+            }
+        }
+        if (options.type == 'GET') {
+            if (params != '') {
+                xhr.open('GET', options.url + '?' + params, true);
+            } else {
+                xhr.open('GET', options.url, true);
+            }
+            xhr.send(null);
+        } else if (options.type == 'POST') {
+            xhr.open('POST', options.url, true);
+            if (!options.contentType) {
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            } else {
+                xhr.setRequestHeader("Content-Type", options.contentType);
+            }
+            xhr.send(params);
+        }
+    }
+    var scriptManager = (function () {
+        let scriptManager = function() {};
+        scriptManager.prototype.addScriptList = function(srcList) {
+            for (let i = 0; i < srcList.length; i++) {
+                scriptManager.prototype.addScript(srcList[i]);
+            }
+        };
+        scriptManager.prototype.addScript = function(src) {
+            let tmpNode = document.createElement("script");
+            tmpNode.src = src;
+            document.body.appendChild(tmpNode);
+        }
+        return new scriptManager();
+    })();
+    var urlManager = (function() {
+        var RouteMode = {
+            HASH: 1,
+            HISTORY: 0,
+        };
+        let urlManager = function() {
+            this.url = {
+                protocol: window.location.protocol || '',
+                hostname: window.location.hostname || '',
+                port: window.location.port || '',
+                pathname: window.location.pathname || '',
+                search: this.getKey(null, 'search'),
+                hash: this.getKey(null, 'hash'),
+            }
+        };
+        urlManager.prototype.routeMode = RouteMode.HASH;
+        urlManager.prototype.init = function(indexList, article) {
+            if (window.location.hostname == '127.0.0.1') {
+                urlManager.prototype.routeMode = RouteMode.HISTORY;
+                let handleUrl = function (href, target, popstate) {
+                    let title = 'f2h2h1\'s blog';
+                    let url = new URL(href);
+                    // console.log(url);
+                    let homepage = window.location.protocol + '//' + window.location.host + '/';
+                    if ((target && target.id == 'homepage') || homepage == href) {
+                        indexList.render();
+                        href = homepage;
+                        let state = { 'title': title, 'href': href };
+                        if (popstate) {
+                            history.replaceState(state, title, href);
+                        } else {
+                            history.pushState(state, title, href);
+                        }
+                        return false;
+                    }
+                    let match = (new RegExp('/article/(.*)\.html', 'g')).exec(url.pathname);
+                    if (match) {
+                        for (let i = articleList.length - 1, len = 0; i >= len; i--) {
+                            if (encodeURI(articleList[i].title) === match[1]) {
+                                title = articleList[i].title;
+                                article.render(title);
+                                let state = { 'title': title, 'href': href };
+                                if (popstate) {
+                                    history.replaceState(state, title, href);
+                                } else {
+                                    history.pushState(state, title, href);
+                                }
+                                return false;
+                            }
+                        }
+                    }
+                    return true;
+                }
+                if (handleUrl(window.location.href, null, false) !== false) {
+                    indexList.render();
+                }
+                document.body.addEventListener('click', function(event) {
+                    let target = event.target;
+                    // 判断是否匹配目标元素
+                    if (target.nodeName.toLocaleLowerCase() === 'a') {
+                        console.log('当前点击的 a 标签: ', target);
+                        // console.log(target.href);
+                        if (handleUrl(target.href, target, false) === false) {
+                            event.preventDefault();
+                        }
+                    }
+                });
+                window.addEventListener('popstate', (event) => {
+                    console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+                    if (event.state && event.state.href) {
+                        handleUrl(event.state.href, null, true)
+                    }
+                });
+            } else {
+                urlManager.prototype.hashChangeFire();
+                window.addEventListener('hashchange', urlManager.prototype.hashChangeFire);
+            }
+        }
+        urlManager.prototype.getKey = function(key, type) {
+            type = type || 'hash';
+            let url;
+            if (type == 'search') {
+                url = location.search.replace(/^\?/, '').split('&');
+            } else {
+                url = location.hash.replace(/^\#/, '').split('&');
+            }
+            var paramsObj = {};
+            for (var i = 0, iLen = url.length; i < iLen; i++) {
+                var param = url[i].split('=');
+                paramsObj[param[0]] = param[1];
+            };
+            if (key) {
+                return paramsObj[key] || '';
+            };
+            return paramsObj;
+        };
+        urlManager.prototype.getTitle = function(type) {
+            type = type || 'article';
+            if (type == 'article') {
+                if (urlManager.prototype.routeMode == RouteMode.HASH) {
+                    return urlManager.prototype.getKey('title');
+                } else {
+                    return '/article/';
+                }
+            } else if (type = 'page') {
+                return '#';
+            } else {
+                return '#';
+            }
+        }
+        urlManager.prototype.createUrl = function(title, type) {
+            type = type || 'article';
+            if (type == 'article') {
+                if (urlManager.prototype.routeMode == RouteMode.HASH) {
+                    return '#title=' + title;
+                } else {
+                    return '/article/' + title + '.html';
+                }
+            } else if (type = 'page') {
+                return '#';
+            } else {
+                return '#';
+            }
+        }
+        urlManager.prototype.hashChangeFire = function() {
+            let title = urlManager.prototype.getTitle();
+            if (title === '') {
+                indexList.render();
+            } else {
+                article.render(title);
+            }
+        }
+        return new urlManager();
+    })();
+    function insertAfter(newElement, targetElement) {
+        var parent = targetElement.parentNode;
+        if (parent.lastChild == targetElement) {
+            // 如果最后的节点是目标元素，则直接添加。因为默认是最后
+            parent.appendChild(newElement);
+        } else {
+            parent.insertBefore(newElement, targetElement.nextSibling);
+            //如果不是，则插入在目标元素的下一个兄弟节点 的前面。也就是目标元素的后面
+        }
+    }
+    function timetostr(timestamp) {
+        return new Date(parseInt(timestamp + '000')).toLocaleString();
+    }
+    function timespan(articleInfo) {
+        let div = document.createElement('div');
+        div.classList.add('timespan');
+        function createCol(str) {
+            let col = document.createElement('div');
+            col.innerHTML = str;
+            return col;
+        }
+        if (articleInfo.createTime) {
+            div.appendChild(createCol('create time: ' + timetostr(articleInfo.createTime)));
+        }
+        if (articleInfo.updateTime) {
+            div.appendChild(createCol('update time: ' + timetostr(articleInfo.updateTime)));
+        }
+        return div;
+        // return 'create time: ' + timetostr(articleInfo.createTime) + '&nbsp&nbsp update time: ' + timetostr(articleInfo.updateTime);
+    }
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+    function findArticleInfo(articleList, title) {
+        for (let i = articleList.length - 1, len = 0; i >= len; i--) {
+            if (articleList[i].title === title) {
+                return articleList[i];
+            }
+        }
+
+        return null;
+    }
+    var IndexList = (function() {
+        var DOM;
+        var DATA;
+        function IndexList(initDOM, initData) {
+            DOM = initDOM;
+            DATA = initData;
+        }
+        IndexList.prototype.render = function() {
+            let content = DOM;
+            let articleList = DATA;
+            content.innerHTML = '';
+            for (let i = 0, len = articleList.length ; i < len; i++) {
+                let div = document.createElement("div");
+                let a = document.createElement("a");
+                let h2 = document.createElement("h2");
+                let hr = document.createElement("hr");
+                let articleTitle = articleList[i].title;
+
+                // h2.innerHTML = articleTitle;
+                // a.appendChild(h2);
+                a.href = urlManager.createUrl(articleTitle);
+                a.innerHTML = articleTitle;
+                h2.appendChild(a);
+                div.appendChild(h2);
+                // div.appendChild(a);
+                div.appendChild(timespan(articleList[i]));
+                div.appendChild(hr);
+                content.appendChild(div);
+            }
+            document.querySelector('title').innerText = 'f2h2h1\'s blog';
+        }
+        return IndexList;
+    })();
+    var ArticleList = (function() {
+        var DOM;
+        var DATA;
+        function ArticleList(initDOM, initData) {
+            DOM = initDOM;
+            DATA = initData;
+        }
+        ArticleList.prototype.render = function() {
+            let content = DOM;
+            let articleList = DATA;
+            content.innerHTML = '';
+            let dt = document.createElement('dt');
+            dt.innerText = 'Article list';
+            content.appendChild(dt);
+            for (let i = 0, len = articleList.length ; i < len; i++) {
+                let dd = document.createElement("dd");
+                let a = document.createElement("a");
+                let articleTitle = articleList[i].title;
+                a.innerHTML = articleTitle;
+                a.href = urlManager.createUrl(articleTitle);
+                dd.appendChild(a);
+                content.appendChild(dd);
+            }
+        }
+        return ArticleList;
+    })();
+    var Article = (function() {
+        var DOM;
+        var DATA;
+        function Article(initDOM, initData) {
+            DOM = initDOM;
+            DATA = initData;
+        }
+        Article.prototype.render = function(title) {
+            let content = DOM;
+            let articleList = DATA;
+            content.innerHTML = '';
+            let url = '/article/' +  title + '.md';
+            // if (urlManager.prototype.routeMode == RouteMode.HISTORY) {
+            //     url = '/' + url;
+            // }
+            ajax({
+                type: 'GET',
+                url: url,
+                dataType: "text",
+                success: function (result) {
+                    content.innerHTML = marked(result);
+                    topFunction();
+                    let tableList = document.getElementsByTagName('table');
+                    for (let i = 0, len = tableList.length; i < len; i++) {
+                        tableList[i].className = 'table';
+                    }
+                    title = decodeURIComponent(title);
+                    let articleInfo = findArticleInfo(articleList, title);
+                    let span = document.createElement("span");
+                    let hr = document.createElement("hr");
+                    content.appendChild(hr);
+                    content.appendChild(timespan(articleInfo));
+                    document.querySelector('title').innerText = title;
+                    if (new Date().getTime() / 1000 - articleInfo.updateTime > 15552000) {
+                        let h1 = content.querySelector('h1');
+                        let newElement = document.createElement("p");
+                        newElement.innerText = "这篇文章最后更新的时间在六个月之前，文章所叙述的内容可能已经失效，请谨慎参考！";
+                        newElement.style.backgroundColor = '#b5b53b';
+                        newElement.style.color = 'black';
+                        if (h1) {
+                            insertAfter(newElement, h1);
+                        } else {
+                            content.insertBefore(newElement, content.firstChild);
+                        }
+                    }
+                },
+                error: function () {
+                    document.querySelector('title').innerText = '404';
+                    content.innerHTML = '<div class="jumbotron"><h1>404</h1><button onclick="history.back()">go back</button></div>';
+                }
+            });
+        }
+        return Article;
+    })();
+    var LinkExchange = (function() {
+        var DOM;
+        function LinkExchange(initDOM) {
+            DOM = initDOM;
+        }
+        LinkExchange.prototype.render = function() {
+            DOM.appendChild((function() {
+                let dt = document.createElement('dt');
+                dt.innerHTML = 'Link exchange';
+                return dt;
+            })());
+            ajax({
+                type: 'GET',
+                url: '/exchangeList.json',
+                dataType: "json",
+                success: function (result) {
+                    let linkExchangeList = result;
+                    for (let i in linkExchangeList) {
+                        DOM.appendChild((function(item) {
+                            let dd = document.createElement('dd');
+                            let a = document.createElement('a');
+                            let desc = item.name;
+                            if (item.desc && item.desc != '') {
+                                desc = item.desc;
+                            }
+                            a.setAttribute('target', '_blank');
+                            a.setAttribute('rel', 'noreferrer');
+                            a.setAttribute('href', item.href);
+                            a.setAttribute('title', desc);
+                            a.innerText = item.name;
+                            dd.appendChild(a);
+                            return dd;
+                        })(linkExchangeList[i]));
+                    }
+                },
+                error: function () {},
+                complete: function () {
+                    DOM.appendChild((function() {
+                        let dd = document.createElement('dd');
+                        dd.innerText = '可以通过 issues 或 邮件 的方式提交你的站点';
+                        return dd;
+                    })());
+                }
+            });
+        }
+        return LinkExchange;
+    })();
+    async function getArticleList() {
+        if (Object.getOwnPropertyNames(articleList).length === 0) {
+            articleList = await (function(){
+                return new Promise((resolve, reject) => {
+                    ajax({
+                        type: "GET",
+                        url: "/articleList.json",
+                        dataType: "json",
+                        success: (data) => {
+                            resolve(data);
+                        },
+                        error: (XMLHttpRequest, textStatus, errorThrown) => {
+                            reject(errorThrown);
+                        }
+                    });
+                });
+            })();
+        }
+        return articleList;
+    }
+    function changeTheme(themeFlg) {
+        var themeBtn = document.getElementById('theme_btn');
+        if (themeFlg) {
+            themeBtn.checked = true;
+            document.body.style.animation = 'l2d 1s 1 normal forwards';
+        } else {
+            themeBtn.checked = false;
+            document.body.style.animation = 'd2l 1s 1 normal forwards';
+        }
+    }
+    async function init() {
+        var themeBtn = document.getElementById('theme_btn');
+        var mql = window.matchMedia('(prefers-color-scheme: dark)');
+        if (mql.matches) {
+            themeBtn.checked = true;
+        } else {
+            themeBtn.checked = false;
+        }
+        mql.addListener(function(e) {
+            changeTheme(e.matches);
+        });
+        themeBtn.addEventListener('click', function() {
+            changeTheme(themeBtn.checked);
+        });
+        (new LinkExchange(document.getElementById('link_exchange'))).render();
+        try {
+            articleList = await getArticleList();
+            let content = document.getElementById('content');
+            var indexList = new IndexList(content, articleList);
+            var article = new Article(content, articleList);
+            urlManager.init(indexList, article);
+            
+            (new ArticleList(document.getElementById('article_list'), articleList)).render();
+        } catch (e) {
+            console.log(e);
+            document.querySelector('title').innerText = '500';
+            content.innerHTML = '<div class="jumbotron"><h1>500</h1><button onclick="window.location.reload()">reload</button></div>';
+            return;
+        }
+        
+    }    
+    application.prototype.run = function() {
+        console.log('application.run');
+        scriptManager.addScriptList([
+            '/static/marked.min.js',
+        ]);
+        ajax({
+            type: "GET",
+            url: "/template.html",
+            dataType: "html",
+            success: (data, XMLHttpRequest) => {
+                let div = document.createElement('div');
+                div.innerHTML = data;
+                document.body.appendChild(div);
+                setTimeout(init, 1000);
+            },
+            error: (XMLHttpRequest, textStatus, errorThrown) => {
+                // reject(errorThrown);
+            }
+        });
+    };
+    return application;
+})();
+window.addEventListener('load', function() {
+    (new Application({})).run();
+});
